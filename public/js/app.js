1f30e0ea03f0a671b5b0b10eab8a413623424787
Adam <adamdhire@gmail.com>
2016-12-10 11:45:42 -0500

    ljdsafasf

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,15 @@ app.config(function($routeProvider) {
   $routeProvider
   .when('/home', {
     controller: 'homeView',
-    templateUrl: 'html/homeView.html'
+    templateUrl: '/html/homeView.html'
   })
   .when('/loggedIn', {
     controller: 'loggedInCtrl',
-    templateUrl: 'html/loggedIn.html'
+    templateUrl: '/html/loggedIn.html'
   })
   .when('/appView', {
     controller: 'showResults',
-    templateUrl: 'html/appView.html'
+    templateUrl: '/html/appView.html'
   })
   .otherwise({
     redirectTo: '/home'
