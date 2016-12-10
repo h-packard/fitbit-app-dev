dca6e231b4530cd4683e0f1934004a16844e378a
Adam <adamdhire@gmail.com>
2016-12-10 11:41:56 -0500

    stuff

--- public/js/app.js
+++ public/js/app.js
@@ -2,21 +2,21 @@ var app = angular.module('myApp', ['ngRoute']);
 
 app.config(function($routeProvider) {
 
-    $routeProvider
-        .when('/home', {
-            controller: 'homeView',
-            templateUrl: 'html/homeView.html'
-        })
-        .when('/loggedIn', {
-            controller: 'loggedInCtrl',
-            templateUrl: 'html/loggedIn.html'
-        })
-        .when('/appView', {
-          controller: 'showResults',
-          templateUrl: 'html/appView.html'
-        })
-        .otherwise({
-            redirectTo: '/home'
-        });
+  $routeProvider
+  .when('/home', {
+    controller: 'homeView',
+    templateUrl: 'html/homeView.html'
+  })
+  .when('/loggedIn', {
+    controller: 'loggedInCtrl',
+    templateUrl: 'html/loggedIn.html'
+  })
+  .when('/appView', {
+    controller: 'showResults',
+    templateUrl: 'html/appView.html'
+  })
+  .otherwise({
+    redirectTo: '/home'
+  });
 
 });
